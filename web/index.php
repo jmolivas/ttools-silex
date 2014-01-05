96be3da25cf76cf1e4dc91313bf035d4770e0208
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-01-05 11:27:59 -0800

    Add how to use on index

--- web/index.php
+++ web/index.php
@@ -2,6 +2,18 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+/**********************************************
+* Copy config/settings.yml.dist to config/settings.yml
+* Update using the keys of your twitter application. 
+*
+* consumer_key: application consumer key
+* consumer_secret: application consumer secret
+* access_token: user access token
+* access_token_secret: user access secret
+*
+* Get the keys you need to register your application at http://dev.twitter.com
+***********************************************/
+
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), [
