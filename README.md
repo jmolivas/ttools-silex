4b86fd96e1b788dde2feb9a19eccefea11932292
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-01-05 02:00:54 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+ttools-silex
+============
+
+TwitterTools Silex demo 
+
+### Usage
+
+* Copy `config/settings.yml.dist` to `config/settings.yml`
+* Update using the keys of your twitter application. To get your keys, you need to register your application at `http://dev.twitter.com`
---
+++ web/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__.'/../views',
+]);
+
+$app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/../config/settings.yml'));
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/timeline', function () use ($app) {
+
+  $config = [
+    'consumer_key'        => $app['config']['twitter']['consumer_key'],
+    'consumer_secret'     => $app['config']['twitter']['consumer_secret'],
+    'access_token'        => $app['config']['twitter']['access_token'] ?: null,
+    'access_token_secret' => $app['config']['twitter']['access_token_secret'] ?: null,
+  ];
+
+  $ttools = new \TTools\App($config);
+  $screen_name = $app['config']['user_timeline']['screen_name'] ?: null;
+  $limit = $app['config']['user_timeline']['limit'] ?: 10;
+  $stream = $ttools->getUserTimeline(null, $screen_name, $limit);
+  
+
+  return $app['twig']->render('timeline.html.twig', [
+      'stream' => $stream,
+  ]);
+
+});
+
+$app->run();
